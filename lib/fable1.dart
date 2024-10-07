70ff7b168fb30027d918e98646c3dc979089c191
Your Name <you@example.com>
2024-10-07 22:01:22 +0700

    เปลี่ยนรูป

--- lib/fable1.dart
+++ lib/fable1.dart
@@ -181,7 +181,7 @@ class _FablePageState extends State<fable01> {
                 borderRadius: BorderRadius.circular(20),
                 child: Stack(
                   children: [
-                    Image.asset('assets/h1.jpg'), // Image path
+                    Image.asset('assets/86.png'), // รูปการ์ตูน
                   ],
                 ),
               ),
